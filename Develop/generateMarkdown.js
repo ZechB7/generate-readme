ecd14a1065bb8778243d06edf2212ca6c37be564
ZechB7 <117792233+ZechB7@users.noreply.github.com>
2023-02-05 19:42:26 -0600

    need to fix renderLicense

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -1,25 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(data) {}
+`![GitHub](https://img.shields.io/github/license/ZechB7/generate-readme)`
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license){
-if(${data.license === MIT}){
-  return `## License
-  This project is licensed under the ${data.license} License.`
-}else if(${data.license === MIT}){
-  return `## License
-  This project is licensed under the ${data.license} License.`
-}else if(${data.license === MIT}){
-  return `## License
-  This project is licensed under the ${data.license} License.`
-}
-}
+function renderLicenseSection(data){
+if(${data.license ==='none'}){
+  return ``
+}else{
+  return `
+  ## License
+  This project is licensed under the ${data.license} License.
+`}}
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
